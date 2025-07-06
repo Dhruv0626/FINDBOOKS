697919775297694fc20cdcc659be8f8a8787c579
Dhruv0626 <dhruvbhavsar6205@gmail.com>
2025-07-06 18:54:41 +0530

    last commit

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,12 @@ const cors = require("cors");
 const PORT = process.env.PORT || 2606;
 const cookieparser = require('cookie-parser');
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieparser());
